63b55ddd43a9da1292e22e4b62b089db53d74586
l0rem1psum <b170007@e.ntu.edu.sg>
2018-10-28 22:59:39 +0800

    Removed uselss files

--- src/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-import java.util.*;
-
-public class Test {
-	public static void main(String[] args) {
-		ArrayList<Integer> l1 = new ArrayList<>();
-		System.out.println(l1.size());
-	}
-}
